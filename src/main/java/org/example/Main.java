49b295bd8efed3ac31b0e9f2b55d9ca8cd44b874
ismaileneskaya@gmail.com <ismaileneskaya@gmail.com>
2024-03-23 01:11:08 +0300

    completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,34 @@ package org.example;
 
 public class Main {
     public static void main(String[] args){
-       Person person1 = new Person();
+      /* Person person = new Person("John", "Doe", 20);
+       System.out.println("Firstname: " + person.getFirstName());
+       System.out.println("LastName: " + person.getLastName());
+       System.out.println("Age: " + person.getAge());
+       System.out.println("Age: " + person.isteen());
+       Person person1 = new Person("John","Doe",18,"Johndoe@gmail.com",55000,"Englend");
+       Person person2 = new Person("Ali","Birinci",25);
+       Person person3 = new Person("şukufe","üçüncü",13,"sukufeucuncu@gmail.com",67000,"Kayseri");
+       Person person4 = new Person("Ahmet","Pehlivan",27);
+        System.out.println("Firstname: " + person3.getFirstName());
+        System.out.println("LastName: " + person3.getLastName());
+        System.out.println("Age: " + person3.getAge());
+        System.out.println("Age: " + person3.isteen());*/
+        Wall wall = new Wall(5,4);
 
+        System.out.println("area= " + wall.getArea());
 
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
     }
 }
+
+
+
+
 
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -6,17 +6,34 @@ public class Person {
 
     int age;
     String email;
-    double salary;
+    int salary;
     String address;
+
 
     public Person(String firstName,String lastName, int age){
         this.firstName=firstName;
         this.lastName=lastName;
         this.age=age;
     }
-    public Person(String email, double salary, String address){
+
+    public Person(String firstName,String lastName, int age,String email, int salary, String address){
+        this(firstName, lastName, age);
         this.email= email;
-        this.salary=salary;
-        this.address=address;
+        this.salary= salary;
+        this.address= address;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public boolean isteen() {
+        return this.age>=13 && this.age<=19;
     }
 }
